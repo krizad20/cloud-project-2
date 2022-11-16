dee22dca2785c9778ace18205450cd153eab3779
krizad20 <vim_intason@hotmail.com>
2022-11-16 22:54:02 +0700

    init

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -30,8 +30,6 @@ const LoginButton = () => {
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Origin': '*',
                     'Access-Control-Allow-Methods': '*',
-                    'Access-Control-Allow-Headers': '*',
-                    
                 }
             })
             .then(function (response) {
